3368e48292362b8c8284c4632a7b7fb70634b177
Dubhs <48168356+Dubhs@users.noreply.github.com>
2023-11-25 08:48:58 -0600

    working copy of usage logic

--- gate class.swift
+++ gate class.swift
@@ -1,46 +1,106 @@
-class Gate {
-	static var allGates: [Gate] = []
-	var name: String
-	var input: [Bool]?
-	var outBuff: Bool?
-//	var cnct: 
-	
-	init(name: String) {
+class Gate
+{
+	init(name: String)
+	{
 		self.name = name
 		Gate.allGates.append(self)
 	}
-	func operate(a: Bool, b: Bool) -> Bool {
-		fatalError("This method should be overridden")
+	
+	static var allGates: [Gate] = []
+	var name: String
+	var input: [Bool] = []
+	var outBuff: Bool = false
+	var cnct: Gate?
+
+	func operate(a: Bool, b: Bool) -> Bool 
+	{
+		fatalError("Override this method")
 	}
 }
 
-class OR: Gate {
-	init() {
-		let className = String(describing: type(of: self))
-super.init(name: "\(className)_\(Gate.allGates.count + 1)")
+class OR: Gate 
+{
+	init() 
+	{
+		let className = 
+		String(describing: type(of: self))
+		
+		super.init(name: "\(className)" +
+		"_\(Gate.allGates.count + 1)")
 	}
-	override func operate(a: Bool, b: Bool) -> Bool {
+
+	override func operate
+	(a: Bool, b: Bool) -> Bool 
+	{
 		return a || b
 	}
 }
 
-class AND: Gate {
-	init() {
-		let className = String(describing: type(of: self))
-		super.init(name: "\(className)_\(Gate.allGates.count + 1)")
+class AND: Gate 
+{
+	init()
+	{
+		let className = 
+		String(describing: type(of: self))
+		
+		super.init(name: "\(className)" +
+		"_\(Gate.allGates.count + 1)")
 	}
-	override func operate(a: Bool, b: Bool) -> Bool {
+
+	override func operate
+	(a: Bool, b: Bool) -> Bool 
+	{
 		return a && b
 	}
 }
 
-// Example Usage
-let orGate = OR()
-let andGate = AND()
-
-for gate in Gate.allGates 
-{
-	print(gate.operate(a: true, b: false))
-	print(gate.name)
+func cnct(gateArray: [Gate], inputs: [Int], out: Int) {
+	for index in inputs {
+		gate[index].cnct = gate[out]
+	}
 }
-print(Gate.allGates)
+
+// Example Usage
+let gates = [AND(), AND(), OR()]
+cnct(gateArray: gates, inputs: [0, 1], out: 2)
+
+// Setting inputs and outputs
+gates[0].input
+	.append(contentsOf: [true, true])
+gates[1].input
+	.append(contentsOf: [true, false])
+
+print(gates[0].input)
+print("•-•------•")
+for gate in Gate.allGates {
+	// Check if there are enough inputs
+	if gate.input.count >= 2
+	{
+		gate.outBuff = gate.operate(
+		a: gate.input[0], b: gate.input[1])
+		print(
+		"gateName: \(gate.name)" +
+		" | result: \(gate.outBuff)")
+	} else {
+		print(
+		"Insufficient inputs for" +
+		"\(gate.name)")
+	}
+
+	if let connectedGate = gate.cnct
+	{
+		print(
+			"Connected to: " +
+			"\(connectedGate.name)")
+		print(connectedGate.input)
+		connectedGate.input.append(
+			gate.outBuff
+		)
+		print(connectedGate.input)
+	} else {
+		print(
+			"No connection for " +
+			"\(gate.name)")
+	}
+	print("•-•------•")
+}
